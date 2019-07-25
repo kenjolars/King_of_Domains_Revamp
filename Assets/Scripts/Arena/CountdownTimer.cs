039834074f23e14c6999d4a1a53206db3a681daa
CooperBraun <52896862+CooperBraun@users.noreply.github.com>
2019-07-25 11:59:59 +1000

    Some updates to maps, menus

---
+++ Assets/Scripts/Arena/CountdownTimer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    //variables
+    public float timeRemaining;
+    Text timer;
+
+    void Awake()
+    {
+        timer = gameObject.GetComponent<Text>();   
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeRemaining = 90;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            //do stuff
+
+        }
+
+        timer.text = timeRemaining.ToString("F0");
+    }
+}
---
+++ Assets/Scripts/Arena/PlayerController.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public int playerNumber = 1;
+    public float Health = 100;
+    bool facingRight;
+    bool grounded = false;
+    float moveSpeed = 3;
+    float jumpForce = 6;
+    Rigidbody2D rigidBody;
+    public GameObject healthBar;
+    Slider hpBar;
+
+    void Awake()
+    {
+        if (playerNumber == 1)
+        {
+            facingRight = true;
+        }
+        if (playerNumber == 2)
+        {
+            facingRight = false;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+        rigidBody = GetComponent<Rigidbody2D>();
+        hpBar = healthBar.GetComponent<Slider>();
+        hpBar.maxValue = Health;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerNumber == 1)
+        {
+            if (grounded && (Input.GetKey(KeyCode.W)))
+            {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+            }
+        }
+        if (playerNumber == 2)
+        {
+            if (grounded && (Input.GetKey(KeyCode.UpArrow)))
+            {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+            }
+        }
+        hpBar.value = Health;
+    }
+    void FixedUpdate()
+    {
+        if (playerNumber == 1)
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+                if (!facingRight)
+                {
+                    Flip();
+                }
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.transform.Translate(Vector2.right * -moveSpeed * Time.deltaTime);
+                if (facingRight)
+                {
+                    Flip();
+                }
+            }
+        }
+        if (playerNumber == 2)
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+                if (!facingRight)
+                {
+                    Flip();
+                }
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.transform.Translate(Vector2.right * -moveSpeed * Time.deltaTime);
+                if (facingRight)
+                {
+                    Flip();
+                }
+            }
+        }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
