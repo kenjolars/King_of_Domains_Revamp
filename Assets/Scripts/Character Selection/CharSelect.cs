3ea90d3749aa0f389ba0da3e603dca290a6ac227
CooperBraun <52896862+CooperBraun@users.noreply.github.com>
2019-07-28 14:24:04 +1000

    Characters now get spawned
    Still got problems though

---
+++ Assets/Scripts/Character Selection/CharSelect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharSelect : MonoBehaviour
+{
+    public static int PlayerNum;
+    public void CharacterSelectFunction (int SelectedNum)
+    {
+        PlayerNum = SelectedNum;
+        Application.LoadLevel("6 Fortmap");
+
+    }
+}
